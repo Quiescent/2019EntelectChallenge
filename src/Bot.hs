8bb56ce3ec66f7f2eaa516bb1c1166cebcc60230
Edward Steere <edward.steere@gmail.com>
2019-05-01 09:44:44 +0000

    Strip out old game playing logic

--- src/Bot.hs
+++ src/Bot.hs
@@ -12,7 +12,6 @@ import Import hiding (round)
 import qualified RIO.Vector.Boxed as V
 import GHC.Generics (Generic)
 import qualified RIO.ByteString.Lazy as B
-import Data.Maybe
 import System.IO
 import System.Random
 import Data.Aeson (decode, withObject, (.:), FromJSON, ToJSON, parseJSON)
@@ -115,123 +114,10 @@ instance FromJSON Cell where
          <*> v .: "y"
          <*> v .: "type"
 
-readGameState :: Int -> RIO App State
+readGameState :: Int -> RIO App (Maybe State)
 readGameState r = do
   stateString <- B.readFile $ "./rounds/" ++ show r ++ "/state.json"
-  let Just state = decode stateString
-  return state
-
-data Direction = East
-               | NorthEast
-               | North
-               | NorthWest
-               | West
-               | SouthWest
-               | South
-               | SouthEast
-  deriving Eq
-
-instance Show Direction where
-  show East      = "E"
-  show NorthEast = "NE"
-  show North     = "N"
-  show NorthWest = "NW"
-  show West      = "W"
-  show SouthWest = "SW"
-  show South     = "S"
-  show SouthEast = "SE"
-
-data Move = DoNothing
-          | Shoot Direction
-          | Move Coord
-          | Dig Coord
-
-instance Show Move where
-  show DoNothing          = "nothing"
-  show (Shoot direction)  = "shoot " ++ show direction
-  show (Move (Coord x y)) = "move " ++ show x ++ " " ++ show y
-  show (Dig (Coord x y))  = "dig " ++ show x ++ " " ++ show y
-
-data CoordWithDirection = CoordWithDirection Coord Direction
-  deriving Eq
-
-hits :: Worm -> Opponent -> V.Vector Direction
-hits (Worm     { weapon = weapon', position = position' })
-     (Opponent { opponentsWorms = worms' }) =
-  V.map (directionFrom position' . opPosition) $ V.filter hits' worms'
-  where
-    range' = range weapon'
-    hits' :: OpponentWorm -> Bool
-    hits' (OpponentWorm { opPosition = opPosition' }) =
-      aligns position' opPosition' &&
-      inRange position' opPosition' range'
-
-directionFrom :: Coord -> Coord -> Direction
-directionFrom (Coord x' y') (Coord x'' y'') =
-  case (y' == y'', x' == x'', x' > x'', y' > y'') of
-    (True,  False, True,  False) -> West
-    (True,  False, False, False) -> East
-    (False, True,  False, True)  -> North
-    (False, True,  False, False) -> South
-    (False, False, True,  True)  -> NorthWest
-    (False, False, False, True)  -> NorthEast
-    (False, False, True,  False) -> SouthWest
-    (False, False, False, False) -> SouthEast
-
-aligns :: Coord -> Coord -> Bool
-aligns (Coord x' y') (Coord x'' y'') =
-  x' == x'' ||
-  y' == y'' ||
-  (abs (x' - x'')) == (abs (y' -y''))
-
-inRange :: Coord -> Coord -> Int -> Bool
-inRange (Coord x' y') (Coord x'' y'') range' =
-  let dx = (fromIntegral (x' - x''))
-      dy = (fromIntegral (y' - y''))
-  in sqrt ((dx ** 2) + (dy ** 2)) < (fromIntegral range')
-
-tryToShoot :: Int -> Player -> Opponent -> Maybe Move
-tryToShoot currentWormId (Player { worms = worms' }) opponent = do
-  currentWorm  <- V.find ((== currentWormId) . wormId) worms'
-  let hits'     = hits currentWorm opponent
-  if V.null hits'
-    then Nothing
-    else fmap Shoot $ hits' V.!? 0
-
-collides :: Coord -> OpponentWorm -> Bool
-collides (Coord x y) (OpponentWorm { opPosition = coord' }) =
-  x == xCoord coord' &&
-  y == yCoord coord'
-
-tryToMoveRandomly :: StdGen -> Int -> Player -> GameMap -> Opponent -> Maybe Move
-tryToMoveRandomly g currentWormId (Player { worms = worms' }) map opponent = do
-  currentWorm  <- V.find ((== currentWormId) . wormId) worms'
-  let wormPosition = position currentWorm
-  let (a, g')      = next g
-  let x            = (xCoord wormPosition) + (mod a 3) - 1
-  let (b, _)       = next g'
-  let y            = (yCoord wormPosition) + (mod b 3) - 1
-  let mapLength    = V.length map
-  if (x < 0 || y < 0 || x >= mapLength || y >= mapLength)
-     then Nothing
-     else if xCoord wormPosition == x && yCoord wormPosition == y
-          then Nothing
-          else do
-    row  <- map V.!? y
-    cell <- row V.!? x
-    translateToMoveOrDig cell
-
-translateToMoveOrDig :: Cell -> Maybe Move
-translateToMoveOrDig (Cell x y "AIR")        = Just $ Move (Coord x y)
-translateToMoveOrDig (Cell x y "DIRT")       = Just $ Dig  (Coord x y)
-translateToMoveOrDig (Cell x y "DEEP_SPACE") = Nothing
-
-makeMove :: StdGen -> State -> Move
-makeMove g (State _ _ _ currentWormId _ myPlayer opponents map) =
-  let opponent = (opponents V.!? 0)
-  in fromJust $ msum [opponent >>= (tryToShoot          currentWormId myPlayer),
-                      opponent >>= (tryToMoveRandomly g currentWormId myPlayer map),
-                      Just DoNothing]
+  return $ decode stateString
 
 readRound :: RIO App Int
 readRound = liftIO readLn
@@ -239,7 +125,6 @@ readRound = liftIO readLn
 startBot :: StdGen -> Int -> RIO App ()
 startBot g roundNumber = do
   round <- readRound
-  state <- readGameState round
-  let (g', g'') = split g
-  liftIO $ putStrLn $ "C;" ++ show roundNumber ++ ";" ++ show (makeMove g' state) ++ "\n"
-  startBot g'' (roundNumber + 1)
+  _     <- readGameState round
+  liftIO $ putStrLn $ "C;" ++ show roundNumber ++ ";nothing\n"
+  startBot g (roundNumber + 1)
