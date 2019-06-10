0f75b745e133847bef1deb9215dfe7ef5e6818ad
Edward Steere <edward.steere@gmail.com>
2019-06-10 13:30:33 +0200

    Make intero use both the lib and tests during development

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode . ((intero-targets . ("2019EntelectChallenge:lib" "2019EntelectChallenge:test:2019EntelectChallenge-test")))))
