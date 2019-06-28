a8fd6bf95322c4e715e7a6d3d55c3512aecb9191
Edward Steere <edward.steere@gmail.com>
2019-06-28 12:55:42 +0200

    Implement more of the skeleton of the test harness

--- test-harness/Main.hs
+++ test-harness/Main.hs
@@ -4,7 +4,12 @@ module Main (main) where
 
 import Import
 
+import Bot
+
 import RIO.List.Partial
+import RIO.Directory
+
+import qualified Data.List as L
 
 import System.Environment
 
@@ -17,5 +22,29 @@ main = do
     let logDirectory = head args
     runRIO app $ runTest logDirectory
 
-runTest :: String -> RIO App ()
-runTest _ = undefined
+runTest :: FilePath -> RIO App ()
+runTest matchLogDirectory = do
+  roundsDirectories <- fmap (filter (L.isPrefixOf "Round")) $ listDirectory matchLogDirectory
+  -- TODO sort the rounds here...
+  simulateAndCheckRounds roundsDirectories
+  
+simulateAndCheckRounds :: [FilePath] -> RIO App ()
+simulateAndCheckRounds (directory:directories) = do
+  initialState <- loadStateForRound directory
+  iter initialState directories
+  where
+    iter :: State -> [FilePath] -> RIO App ()
+    iter _ _ = undefined
+
+loadStateForRound :: FilePath -> RIO App State
+loadStateForRound _ = undefined
+
+loadThisPlayersCommand :: FilePath -> RIO App Move
+loadThisPlayersCommand _ = undefined
+
+loadThatPlayersCommand :: FilePath -> RIO App Move
+loadThatPlayersCommand _ = undefined
+
+tickState :: Move -> Move -> State -> RIO App State
+tickState _ _ _ = undefined
+
