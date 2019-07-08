f3f21db17774605d15e38c37c7597596d214e7e5
Edward Steere <edward.steere@gmail.com>
2019-07-08 12:43:55 +0200

    Implement script to build and profile the system

---
+++ profile.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+echo "======================================================================"
+echo "===Please note that this should be run from the root of the repo!!===="
+echo "======================================================================"
+set -e
+
+stack install --profile --local-bin-path bin
+
+./bin/Profile-exe "../2019-Worms/game-runner/match-logs/2019.07.02.12.47.48/"
