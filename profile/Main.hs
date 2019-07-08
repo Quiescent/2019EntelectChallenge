bcaa91492f150d17b8b8a48e4451885aa2de69b7
Edward Steere <edward.steere@gmail.com>
2019-07-08 12:34:28 +0200

    Implement `runDataSet'

--- profile/Main.hs
+++ profile/Main.hs
@@ -2,7 +2,9 @@ module Main
   where
 
 import Import
+import Simulate
 import System.Environment
+import qualified System.IO as IO
 
 main :: IO ()
 main = do
@@ -14,4 +16,11 @@ main = do
   runRIO app $ runDataSet dataSetDirectory
 
 runDataSet :: FilePath -> RIO App ()
-runDataSet = undefined
+runDataSet matchLogsDirectory = do
+  result <- withRoundsDirectories matchLogsDirectory runSearchForEachRound
+  case result of
+    Success           -> liftIO $ IO.putStrLn ("Ran search for each round in: " ++ matchLogsDirectory)
+    (Failure message) -> liftIO $ IO.putStrLn message
+
+runSearchForEachRound :: [FilePath] -> RIO App Result
+runSearchForEachRound = undefined
