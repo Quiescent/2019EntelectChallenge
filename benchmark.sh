4eb79dea343b4f7e01e0ec03ee84b23200ddb74d
Edward Steere <edward.steere@gmail.com>
2019-07-09 06:03:00 +0000

    Fix output of benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -10,13 +10,13 @@ stack install --local-bin-path bin
 echo "======================================================================"
 echo "=======================Start of Benchmarking=========================="
 echo "======================================================================"
-OUTPUT_FILE="benchmarking.out"
+OUTPUT_FILE="benchmarking.bench"
 ./bin/Benchmark-exe "matches/round-1/2019.07.07.13.24.43/" > "$OUTPUT_FILE"
 
 CURRENT_COMMIT=$(git log --oneline | head -1 | cut -d' ' -f1)
 CURRENT_MACHINE=$(uname)
-OUTPUT_DIRECTORY="profiling/$CURRENT_MACHINE"
+OUTPUT_DIRECTORY="benchmarking/$CURRENT_MACHINE"
 mkdir -p $OUTPUT_DIRECTORY
-mv "$OUTPUT_FILE" "$OUTPUT_DIRECTORY/$CURRENT_COMMIT.prof"
+mv "$OUTPUT_FILE" "$OUTPUT_DIRECTORY/$CURRENT_COMMIT.bench"
 
-echo "Done.  You can find the results in $OUTPUT_DIRECTORY/$CURRENT_COMMIT.prof"
+echo "Done.  You can find the results in $OUTPUT_DIRECTORY/$CURRENT_COMMIT.bench"
