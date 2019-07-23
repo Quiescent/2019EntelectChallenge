c97ccba1a6cc56a5438e66e6a8b453b642bd2aad
Edward Steere <edward.steere@gmail.com>
2019-07-23 17:03:46 +0200

    Update profile and benchmark to use the latest round files

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script will only work for the round which the engine was
+# designed for.  If you try to run it for a different round then we
+# wont be able to deserialise the state.
+
 echo "======================================================================"
 echo "===Please note that this should be run from the root of the repo!!===="
 echo "======================================================================"
@@ -11,7 +15,7 @@ echo "======================================================================"
 echo "=======================Start of Benchmarking=========================="
 echo "======================================================================"
 OUTPUT_FILE="benchmarking.bench"
-./bin/Benchmark-exe "matches/round-1/2019.07.07.13.24.43/" > "$OUTPUT_FILE"
+./bin/Benchmark-exe "matches/round-2/2019.07.21.22.42.46" > "$OUTPUT_FILE"
 
 CURRENT_COMMIT=$(git log --oneline | head -1 | cut -d' ' -f1)
 CURRENT_MACHINE=$(uname)
--- profile.sh
+++ profile.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script will only work for the round which the engine was
+# designed for.  If you try to run it for a different round then we
+# wont be able to deserialise the state.
+
 echo "======================================================================"
 echo "===Please note that this should be run from the root of the repo!!===="
 echo "======================================================================"
@@ -10,7 +14,7 @@ stack install --profile --local-bin-path bin
 echo "======================================================================"
 echo "==========================Start of Profiling=========================="
 echo "======================================================================"
-./bin/Profile-exe "matches/round-1/2019.07.07.13.24.43/" +RTS -p
+./bin/Profile-exe "matches/round-2/2019.07.21.22.42.46" +RTS -p
 
 CURRENT_COMMIT=$(git log --oneline | head -1 | cut -d' ' -f1)
 CURRENT_MACHINE=$(uname)
