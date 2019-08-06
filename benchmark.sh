0dd9b0cdcb1d648c49b8f6e6e52fcb071a87b41d
Edward Steere <edward.steere@gmail.com>
2019-08-06 08:49:39 +0000

    Parameterise the benchmarking script

--- benchmark.sh
+++ benchmark.sh
@@ -15,12 +15,13 @@ echo "======================================================================"
 echo "=======================Start of Benchmarking=========================="
 echo "======================================================================"
 OUTPUT_FILE="benchmarking.bench"
-./bin/Benchmark-exe "matches/round-2/2019.07.21.22.42.46" +RTS -N2 -RTS > "$OUTPUT_FILE"
+./bin/Benchmark-exe $* +RTS -N2 -RTS > "$OUTPUT_FILE"
 
 CURRENT_COMMIT=$(git log --oneline | head -1 | cut -d' ' -f1)
 CURRENT_MACHINE=$(uname)
 OUTPUT_DIRECTORY="benchmarking/$CURRENT_MACHINE"
 mkdir -p $OUTPUT_DIRECTORY
-mv "$OUTPUT_FILE" "$OUTPUT_DIRECTORY/$CURRENT_COMMIT.bench"
+MATCH_ARG_STRING=$(echo "$*" | grep -o "[0-9][0-9][0-9][0-9].*" | sed -e 's/ /-/g')
+mv "$OUTPUT_FILE" "$OUTPUT_DIRECTORY/$CURRENT_COMMIT-$MATCH_ARG_STRING.bench"
 
 echo "Done.  You can find the results in $OUTPUT_DIRECTORY/$CURRENT_COMMIT.bench"
