4ce61e06b7811f91a3099be2ac77f08daa55d72c
Edward Steere <edward.steere@gmail.com>
2019-08-17 15:00:52 +0000

    Add a profiling mode where we just run the same round a bunch

--- profile/Main.hs
+++ profile/Main.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main
   where
 
@@ -6,11 +13,14 @@ import Simulate
 import Bot
 
 import RIO.List
+import RIO.List.Partial
 import Control.Concurrent
 import System.Random
 import System.Environment
 import Data.Maybe
 import qualified System.IO as IO
+
+import Prelude (read, putStrLn)
 
 main :: IO ()
 main = do
@@ -19,29 +29,44 @@ main = do
   args   <- getArgs
   guard (length args >= 1)
   let dataSetDirectory = head args
-  let startFrom = headMaybe $ tail args
+  let startFrom        = headMaybe $ tail args
+  let repeatTimes      = (headMaybe $ tail args) >>= tailMaybe >>= tailMaybe
   when (isJust startFrom) $
     liftIO $ putStrLn ("Starting search from: " ++ show (fromJust startFrom))
-  runRIO app $ runDataSet dataSetDirectory startFrom
+  runRIO app $ runDataSet dataSetDirectory startFrom repeatTimes
 
-runDataSet :: FilePath -> Maybe String -> RIO App ()
-runDataSet matchLogsDirectory startFrom = do
-  withRoundsDirectories matchLogsDirectory (runSearchForEachRound startFrom)
+runDataSet :: FilePath -> Maybe String -> Maybe String -> RIO App ()
+runDataSet matchLogsDirectory startFrom repeatTimes = do
+  withRoundsDirectories matchLogsDirectory (runSearchForEachRound startFrom repeatTimes)
   liftIO $ IO.putStrLn ("Ran search for each round in: " ++ matchLogsDirectory)
 
-runSearchForEachRound :: Maybe String -> [FilePath] -> RIO App ()
-runSearchForEachRound _         []           = error "No round directories to profile over."
-runSearchForEachRound startFrom directories  =
+runSearchForEachRound :: Maybe String -> Maybe String -> [FilePath] -> RIO App ()
+runSearchForEachRound _         _           []           = error "No round directories to profile over."
+runSearchForEachRound startFrom repeatTimes directories  =
   let directories' =
         if isJust startFrom
         then dropWhile (not . isInfixOf (fromJust startFrom)) directories
         else directories
-  in if (length directories' < 1)
-     then (liftIO (putStrLn ("User error: specified directory has less than 1 rounds after starting from: " ++
+      repeatMode  = isJust repeatTimes
+      repeatCount = read $ fromJust repeatTimes
+  in if repeatMode
+     then do
+       let directory = head directories'
+       state         <- fmap fromJust $ loadStateForRound directory
+       gen           <- liftIO getStdGen
+       treeChannel   <- liftIO newComms
+       stateChannel  <- liftIO newComms
+       _             <- liftIO $ forkIO (iterativelyImproveSearch gen state SearchFront stateChannel treeChannel)
+       forM_ [(1::Int)..repeatCount] $ \ i -> do
+         liftIO $ logStdErr $ "Round " ++ show i ++ "/" ++ show repeatCount
+         _      <- liftIO $ searchForAlottedTime state treeChannel
+         liftIO $ writeComms stateChannel ((fromMoves doNothing doNothing), state)
+     else if (length directories' < 1)
+          then (liftIO (putStrLn ("User error: specified directory has less than 1 rounds after starting from: " ++
                              case startFrom of
                                Just x -> x
                                _      -> "the beginning.")))
-     else do
+          else do
     let directory     = head directories'
     let directories'' = tail directories'
     state         <- fmap fromJust $ loadStateForRound directory
@@ -52,7 +77,7 @@ runSearchForEachRound startFrom directories  =
     mapM_ (\ directory' -> do
               liftIO $ logStdErr $ "Profiling directory: " ++ directory'
               state' <- fmap fromJust $ loadStateForRound directory'
-              _      <- liftIO $ searchForAlottedTime state treeChannel
+              _      <- liftIO $ searchForAlottedTime state' treeChannel
               -- Simulate the worst case scenario.  Both players do
               -- nothing and we don't have it in the tree.
               liftIO $ writeComms stateChannel ((fromMoves doNothing doNothing), state'))
