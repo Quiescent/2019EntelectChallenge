b74d3a9a6a2010b359dcf6bb32171d246bb9e4a6
Edward Steere <edward.steere@gmail.com>
2019-09-03 06:36:08 +0000

    Revert "Cache all unpacked coordinates"
    This reverts commit adac6f98fe866ca2e671ac24e6916339a6acbe66.

--- src/Bot.hs
+++ src/Bot.hs
@@ -11,8 +11,6 @@ module Bot
 import Import
 import Lava
 
-import qualified RIO.Vector.Unboxed as UV
-import qualified RIO.Vector.Unboxed.Partial as UVP
 import qualified Data.IntMap.Lazy as IM
 import qualified RIO.Vector.Boxed as V
 import qualified RIO.Vector.Boxed.Partial as PV
@@ -1082,10 +1080,8 @@ toCoord xCoord yCoord =
 -- Consider whether to use applicative here and get rid of the tuple
 fromCoord :: Coord -> (Int, Int)
 fromCoord xy =
-  coords UVP.! xy
-  where
-    coords           = UV.fromList $ map (flipTuple . (flip divMod) mapDim) [0..(mapDim * mapDim)]
-    flipTuple (a, b) = (b, a)
+  case (divMod xy mapDim) of
+    (y', x') -> (x', y')
 
 data Weapon = Weapon { damage :: Int,
                        range :: Int }
